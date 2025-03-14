a13086a0c5ff583be31bc720c99f5e9ba9efb234
Venkatarao <venkaktaraodama386@example.com>
2025-03-14 11:05:13 +0530

    first commit

---
+++ indexing.py
@@ -0,0 +1,16 @@
+#my_list = [10,20,30,40,50,60,70,80,90,100]
+
+#print(my_list[0])
+#print(my_list[1])
+#print(my_list[8])
+
+#print(my_list[-1])
+
+#print(my_list[-7])
+
+# set_fruits = {"apple", "banana", "cherry"}
+# set_fruits.add("orange")
+# print(set_fruits)
+
+for i in range(1,21):
+    print (i , end=" ")
